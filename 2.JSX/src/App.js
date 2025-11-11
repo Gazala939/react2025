a6ed80cf7588b86e460fb0aa7386b88137a2f524
Gazala939 <Gazalajamadar786@gmail.com>
2025-11-11 12:47:16 +0530

    JSX program

--- 2.JSX/src/App.js
+++ 2.JSX/src/App.js
@@ -3,6 +3,15 @@ function App() {
     let name = "Gazala";
     const date = new Date().toLocaleDateString()
     const time = new Date().toLocaleTimeString()
+    const hour = new Date().getHours()
+    let greeting = " "
+    if(hour >=1 && hour<12){
+        greeting = 'Good morning'
+    }else if (hour >=12 && hour <19){
+        greeting = 'Good Afternoon'
+    }else{
+        greeting = "Good Night"
+    }
 
     const heading = {
         color:'red',
@@ -12,12 +21,14 @@ function App() {
     }
   return (
     <>
-      <h1> Welcome React </h1>
+      <h1> Welcome  </h1>
       <h2 style = {heading}> My name is {name}</h2>
       <h3> Date :{date}</h3>
       <h3> Time : {time}</h3>
+      <h2> {greeting}!!!</h2>
     </>
   );
 }
 
 export default App;
+
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; /// create the components
+import ReactDOM from 'react-dom/client';/// gives the tools to show componemet
 
-import App from './App';
+import App from './App';  // UI component u created
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
