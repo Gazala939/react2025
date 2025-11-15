2538f01c959ff93eedfd9a3017d2b5aaabb55b4d
Gazala939 <Gazalajamadar786@gmail.com>
2025-11-15 17:07:44 +0530

    props and dynamic component rendering

--- 4.Props_Movie_Mini_Project/src/index.js
+++ 4.Props_Movie_Mini_Project/src/index.js
@@ -6,9 +6,9 @@ import App from './App';  // UI component u created
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode> 
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
 
 
