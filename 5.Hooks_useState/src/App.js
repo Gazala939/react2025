7a069ed31b4d1afbed243dcc2413c855f621a66c
Gazala939 <Gazalajamadar786@gmail.com>
2025-11-16 08:26:01 +0530

    5.Hooks_useState

--- 5.Hooks_useState/src/App.js
+++ 5.Hooks_useState/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './app.css';
 import{ useState} from 'react'
 function App() {
+
+    // useState is an Array.applyIt takes 2 parameters
+    // first parameter: current Date
+    // second parametre: update DataTransfer
+    // useState function parameter is default initial value.
+    // i.e default initial value indirectly assign to the first parametre
+
     let [counter,setCounter] = useState(0);
 
     const buttonEvent = () => {
