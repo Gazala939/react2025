547a890a04bdcceaf1c5102fcbbafaf81e0c4143
Gazala939 <Gazalajamadar786@gmail.com>
2025-11-16 11:14:41 +0530

    7.React_Form

--- 7.React_Form/src/App.js
+++ 7.React_Form/src/App.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import { useState } from "react"
 function App() {
-    const [name, setName] = useState('')
-    const [firstName,setFirstName] = useState('')
+    const [fname, setFName] = useState('');
+    const [lname, setLName] = useState('');
+
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
     
-    const InputEvent =(event)=>{
-        console.log('Event call :', event.target.name,event.target.value);
-        setName(event.target.value);
+    const InputEvent1 =(event)=>{
+        setFName(event.target.value);
+    }
+    const InputEvent2 =(event)=>{
+        setLName(event.target.value);
     }
     const showResult=(event) =>{
         event.preventDefault();    
-        setFirstName(name);
+        setFirstName(fname);
+        setLastName(lname);
     };
+
+    const clearText = ()=>{
+        setFName('');
+        setFirstName('');
+        setLName('');
+        setLastName('');
+    }
  
   return (
     <>
     <form onSubmit = {showResult}>
     <div>
-      <h1> Welcome {firstName} </h1>
-      <input type ="text"
-      placeholder ="Enter ur name"
-      onChange={InputEvent}
+      <h1> Welcome {firstName} {lastName}</h1>
+
+      <input 
+      type ="text"
+      placeholder ="Enter First name"
+      onChange={InputEvent1}
       name = "firstName"></input>
       <br></br>
       <br></br>
-      <button type="submit" /*onClick={showResult}> */>Click Me</button>
-      <button type = "reset">Clear Text</button>
+
+      <input 
+      type ="text"
+      placeholder ="Enter Last name"
+      onChange={InputEvent2}
+      name = "LastName"></input>
+      <br></br>
+
+      <br></br>
+      <button type="submit">Click Me</button>
+      <button type = "reset" onClick={clearText}>Clear Text</button>
       </div>
       </form>
     </>
