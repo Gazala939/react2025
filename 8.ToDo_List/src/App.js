f6bff261c98486b4b87d39ea27959ad78be4ab81
Gazala939 <Gazalajamadar786@gmail.com>
2025-11-17 06:40:35 +0530

    8.add_button and List

--- 8.ToDo_List/src/App.js
+++ 8.ToDo_List/src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css'
 function App() {
-    
+    const[item,setItem] = useState(' ');
+    const [list,setList] = useState([])
+
+    const itemEvent=(event) =>{
+        setItem(event.target.value)
+
+    }
+    const addItem =()=>{
+        setList((oldItems)=>{
+            return[...oldItems,item]
+        });
+
+        setItem('');
+    }
   return (
     <div className = 'main-div'>
         <div className = 'center-div'>
             <br></br>
             <h1> To Do List</h1>
             <br></br>
-            <input type = "text" placeholder="Add a Item"></input>
-            <button>+</button>
+            <input type = "text" 
+            placeholder="Add a Item" 
+            onChange={itemEvent}
+            value={item}>
+            </input>
+            <button onClick = {addItem}>+</button>
             <ol>
-                <li> 6:00am wake Up</li>
-                <li> 6:30am youga</li>
-                <li> 7:00 breakfast</li>
+               {
+                list.map((val,index)=>{            
+                    return <li key={index}>{val}</li>
+                })
+               }
             </ol>
             
         </div>
